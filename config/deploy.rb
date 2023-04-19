fe06254dbe19fd6a86c4cb3caf5476c6dadc3a47
JasonWu <me@ijasonwu.com>
2023-04-19 22:40:49 +0800

    push

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,6 +10,7 @@ set :deploy_to, "/home/deploy/#{fetch :application}"
 
 append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', '.bundle', 'public/system', 'public/uploads'
 
+append :linked_files, "config/database.yml", 'config/master.key'
 # Only keep the last 5 releases to save disk space
 set :keep_releases, 5
 
