7b1c9be93243fd38f8bc31b9c584a9ee8943a5b8
JasonWu <me@ijasonwu.com>
2023-10-04 20:21:59 +0800

    disable csrf

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,6 @@
 class ApplicationController < ActionController::Base
+  # disable the CSRF token
+  protect_from_forgery with: :null_session
+  # disable the CSRF token
+  skip_before_action :verify_authenticity_token
 end
