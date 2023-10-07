7409c5ec672f789c09c0878f67be9b86c7e4a18e
JasonWu <me@ijasonwu.com>
2023-10-07 14:53:07 +0800

    push

--- app/controllers/dy_controller.rb
+++ app/controllers/dy_controller.rb
@@ -7,16 +7,18 @@ class DyController < ApplicationController
     access_token = AccessToken.get_or_refresh_token(false )
     # 构建请求参数
     request_payload = {
-      jump_wxa: {
-        path: "/pages/addUser/index",
-        query: "code=#{params[:id]}",
-        env_version: "release"
-      },
+      # jump_wxa: {
+      #   path: "/pages/addUser/index",
+      #   query: "code=#{params[:id]}",
+      #   env_version: "release"
+      # },
+      path: "/pages/addUser/index",
+      query: "code=#{params[:id]}",
       expire_type: 1,
       expire_interval: 30
     }
     response = RestClient.post(
-      "https://api.weixin.qq.com/wxa/generatescheme?access_token=#{access_token}",
+      "https://api.weixin.qq.com/wxa/generate_urllink?access_token=#{access_token}",
       request_payload.to_json,
       content_type: :json,
       accept: :json
@@ -26,7 +28,7 @@ class DyController < ApplicationController
     if response_data['errcode'] == 0
       # 请求成功，返回openlink参数
       # render json: { openlink: response_data['openlink'] }, status: :ok
-      @cardsche = response_data['openlink']
+      @cardsche = response_data['url_link']
     elsif response_data['errcode'] == 40001
       AccessToken.get_or_refresh_token(true )
       p "触发强制刷新ACCESS_TOKEN"
@@ -36,6 +38,10 @@ class DyController < ApplicationController
       # render json: { errcode: response_data['errcode'] }, status: :unprocessable_entity
       @cardsche = "errorCode#{response_data['errcode']}"
     end
+
+    p @cardsche
+
+    redirect_to @cardsche, allow_other_host: true
 
   end
 end
